a3dbc7d881b98b2dc26c79ebe880f0897fa3bf28
yusran jonjo <msaidibrahim11@gmail.com>
2021-03-22 14:26:52 +0700

    6. selesai namun masih banyak tambahan

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const barsMenu = document.querySelector('.bars-menu');
 const daftarMenu = document.querySelector('.daftar-menu');
 const listMenu = document.querySelectorAll('.daftar-menu li');
 const iconBars = document.querySelector('.fa-bars');
+const header = document.querySelector('.header');
+
+let lastScrollTop = 0;
+window.addEventListener('scroll', function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        header.style.top = '-180px';
+    } else {
+        header.style.top = '0';
+    }
+    lastScrollTop = scrollTop;
+})
 
 barsMenu.addEventListener('click', function () {
     daftarMenu.classList.toggle('menus');
